9688652c1655ebdceca3d192e07de1199db470d9
tarunyendu-developer <tarunyendu2807@gmail.com>
2026-04-17 19:34:04 +0530

    Added SecurityConfig to allow all requests temporarily

---
+++ src/main/java/com/bank/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable()) // disable CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // allow all APIs
+                );
+
+        return http.build();
+    }
+}
