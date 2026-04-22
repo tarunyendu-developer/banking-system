c8ba117c728d65e0e408c05e2d12161ea13d27e9
tarunyendu-developer <tarunyendu2807@gmail.com>
2026-04-22 19:15:20 +0530

    Added Swagger UI with JWT authentication support

---
+++ src/main/java/com/bank/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.bank.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Banking System API")
+                        .version("1.0")
+                        .description("Backend APIs for Banking System")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
